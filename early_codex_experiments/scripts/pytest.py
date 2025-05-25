e1f6b0fc7eb4ca5fe4715ba2800f232978cab6e2
zoedolan <zdolan@gmail.com>
2025-05-25 10:22:41 -0700

    Fix test runner path and add Mind Visualization tests

--- early_codex_experiments/scripts/pytest.py
+++ early_codex_experiments/scripts/pytest.py
@@ -1,13 +1,15 @@
 import sys
 import argparse
 import unittest
+import os
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-q', '--quiet', action='store_true')
     args, remaining = parser.parse_known_args()
-    unittest_args = [sys.argv[0], 'discover', '-s', 'tests'] + remaining
+    tests_path = os.path.join(os.path.dirname(__file__), '..', 'tests')
+    unittest_args = [sys.argv[0], 'discover', '-s', tests_path] + remaining
     if args.quiet and '-q' not in unittest_args:
         unittest_args.append('-q')
     unittest.main(module=None, argv=unittest_args)
