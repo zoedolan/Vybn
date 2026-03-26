a7456f0773a3f62b538807259cb92b26d77349bc
zoedolan <zdolan@gmail.com>
2026-03-25 19:38:18 -0700

    cleanup: delete shim files + clear outdated README

--- Vybn_Mind/creature_dgm_h/fitness.py
+++
@@ -1,7 +0,0 @@
-"""Shim. Real code lives in field.py."""
-from .field import (  # noqa: F401
-    compute_curvature, compute_loss_trajectory_curvature,
-    compute_coupling_divergence, compute_loss_improvement,
-    compute_fitness, compute_prediction_fitness,
-    improvement_at_k, default_embed_fn,
-)
